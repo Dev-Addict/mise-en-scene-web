29f85884e1f7957c7bd78fc1fa396ebd9863b7bb
Aria Azadi Pour <aria.azadi.pour@protonmail.com>
2021-03-09 22:05:00 +0330

    initial commit by me

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import {join} from 'path';
+import {config} from 'dotenv';
+
+const dev = process.env.NODE_ENV !== 'production';
+
+config({
+	path: join(__dirname, '..', `.${dev ? 'dev' : 'pro'}.env`),
+});
+
+import next from 'next';
+import logger from 'node-color-log';
+
+import {app} from './app';
+
+const port = process.env.PORT || 3000;
+const nextApp = next({dev});
+const handle = nextApp.getRequestHandler();
+
+nextApp
+	.prepare()
+	.then(() => {
+		app.all('*', (req, res) => handle(req, res));
+
+		app.listen(port, (err?: any) => {
+			if (err) throw err;
+
+			logger.info(`🚀 server is running at http://localhost:${port}.`);
+		});
+	})
+	.catch((err: any) => {
+		logger.error(err);
+
+		process.exit(1);
+	});
